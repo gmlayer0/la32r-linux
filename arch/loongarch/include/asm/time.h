2b930817d20493421c7776132b3c2901a5de2249
dofingert <dofingert@gmail.com>
2023-05-26 00:21:53 +0800

    support running in qemu

--- arch/loongarch/include/asm/time.h
+++ arch/loongarch/include/asm/time.h
@@ -41,8 +41,9 @@ static inline unsigned int calc_const_freq(void)
 #define LOONGARCH32_FREQ_REG 0x9fd0f030
 static inline unsigned int calc_const_freq(void)
 {
-    unsigned int freq = *(int *)LOONGARCH32_FREQ_REG;
-	return freq;
+    // unsigned int freq = *(int *)LOONGARCH32_FREQ_REG;
+	// return freq;
+	return 200000000;
 }
 #endif
 
--- la_build.sh
+++ la_build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-export CROSS_COMPILE=~/install-32-glibc-loongarch-novec-reduce-linux-5-14/bin/loongarch32-linux-gnu-
+export CROSS_COMPILE=loongarch32r-linux-gnusf-
 export ARCH=loongarch
 OUT=la_build
 
 if [ ! -d la_build ] ;then
     mkdir la_build
-    make la32_defconfig O=${OUT}
 fi
 
 echo "----------------output ${OUT}----------------"
 
+make la32_defconfig O=${OUT}
 make menuconfig O=${OUT}
 make vmlinux -j  O=${OUT} 2>&1 | tee -a build_error.log
+cp la_build/vmlinux ../qemu_work/vmlinux
